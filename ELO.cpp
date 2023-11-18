6f14714ee2a381136250f95130dbb86f84c7412f
milikrans <milikrans05@gmail.com>
2023-11-18 17:33:18 -0600

    version semi estable v2

--- ELO.cpp
+++ ELO.cpp
@@ -2,35 +2,40 @@
 #include <cstdlib>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
 int main (){
-//semilla random  
+    //semilla random  
     srand(time(NULL));
-//declaracion de variables
+    //declaracion de variables
     int culpable, arma, location, aux, memoria, tiempo;
     bool continuar = true;
-//inicialozacion de mapas    
+    string basura ="basura";
+    //inicialozacion de mapas    
     char tutorial [6][18]{{"                 "},{" +----+   +----+ "},{" |    |---|????| "},{" |  I |---|    | "},{" +----+   +----+ "},{"                 "}};
     int tutorialAncho = sizeof(tutorial) / sizeof(tutorial[0]);
     int tutorialAlto = sizeof(tutorial[0]) / sizeof(tutorial[0][0]);
     
-    char mapa[16][27]{{"                          "},{" +----+   +----+   +----+ "},{" |    |---|    |---|    | "},{" |    |---|    |---|    | "},{" +----+   +----+   +----+ "},{"  | |       | |      | |  "},{" +----+   +----+     | |  "},{" |    |---|    |-----+ |  "},{" |    |---|    |-------+  "},{" +----+   +----+          "},{"            | |           "},{" +----------------------+ "},{" |                      | "},{" |                      | "},{" +----------------------+ "},{"                          "}};
+    char mapa[16][27]{{"                          "},{" +----+   +----+   +----+ "},{" | Hp |---| Tc |---| C  | "},{" |    |---|    |---|    | "},{" +----+   +----+   +----+ "},{"  | |       | |      | |  "},{" +----+   +----+     | |  "},{" | Dc |---| V  |-----+ |  "},{" |    |---|  I |-------+  "},{" +----+   +----+          "},{"            | |           "},{" +----------------------+ "},{" |          J           | "},{" |                      | "},{" +----------------------+ "},{"                          "}};
     int mapaAncho = sizeof(mapa) / sizeof(mapa[0]);
     int mapaAlto = sizeof(mapa[0]) / sizeof(mapa[0][0]);
-//inicializacion de apecto aleatorio    
+    //inicializacion de apecto aleatorio    
     culpable = rand() % 3;
     arma = rand() % 3;
-//inicio de arbol de deciciones    
+    //inicio de arbol de deciciones    
     location = 0;
     memoria = location;
     tiempo = 0;
     cout<<"dialogo introductorio"<<endl;
-//cin>>location;
-    do
-    {
-        //clock sprite sheet
+    cin>>location;
+    cin.ignore(1000, '\n');
+
+  
+    //tutorial
+    do{
+        //clock atgain
         switch (tiempo)
         {
         case 0:
@@ -257,6 +262,305 @@ int main (){
         default:
             break;
         }
+        //map printing
+        for (int i = 0; i < tutorialAncho; i++){
+            for (int j = 0; j < tutorialAlto; j++){
+                cout<<tutorial[i][j];
+            }
+            cout<<endl;
+        }   
+        //opciones
+        switch (location)
+        {
+        case 1:
+            memoria=location;
+            cout<<"variacion cinematica 1"<<endl;
+            tiempo++;
+            tutorial[3][4]=' ';
+            tutorial[3][13]='I';
+            location=5;
+            break;
+        case 2:
+            memoria=location;
+            cout<<"variacion cinematica 2"<<endl;
+            tiempo++;
+            tutorial[3][4]=' ';
+            tutorial[3][13]='I';
+            location=5;
+            break;
+        case 3:
+            memoria=location;
+            cout<<"variacion cinematica 3"<<endl;
+            tiempo++;
+            tutorial[3][4]=' ';
+            tutorial[3][13]='I';
+            location=5;
+            break;
+        case 4:
+            memoria=location;
+            cout<<"variacion cinematica 4"<<endl;
+            tiempo++;
+            tutorial[3][4]=' ';
+            tutorial[3][13]='I';
+            location=5;
+            break;
+        case 5:
+            memoria=location;
+            cout<<"continuacion cinematica"<<endl;
+            
+            getline (cin, basura);
+            cin.ignore(1000, '\n');
+            cout<<"final cinematica"<<endl;
+
+            tiempo=0;
+            continuar=false;
+            break;
+        default:
+            cout<<"seleccion invalida"<<endl;
+            location=memoria;
+            break;
+        }
+    } while (continuar == true);
+    cout<<"final tutorial";
+    location = 0;
+    //actual game
+    do
+    {
+    //clock sprite sheet
+        switch (tiempo)
+        {
+        case 0:
+            cout<<" +-------------+ \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 1:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *----->| \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 2:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 3:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |<-----*      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 4:
+            cout<<" +-------------+ \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *-->   | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 5:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      *-->-->| \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 6:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      *-->   | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 7:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |<-----*-->   | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 8:
+            cout<<" +-------------+ \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 9:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      *----->| \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 10:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |      *      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 11:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |<-----*      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+         case 12:
+            cout<<" +-------------+ \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |   <--*      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n"; 
+            break;
+        case 13:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |   <--*----->| \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 14:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |   <--*      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      v      | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 15:
+            cout<<" +-------------+ \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |<--<--*      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        case 16:
+            cout<<" +-------------+ \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      ^      | \n";
+            cout<<" |      |      | \n";
+            cout<<" |      *      | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" |             | \n";
+            cout<<" +-------------+ \n";
+            break;
+        default:
+            break;
+        }
+    //map printing
+        for (int i = 0; i < mapaAncho; i++){
+        for (int j = 0; j < mapaAlto; j++){
+            cout<<mapa[i][j];
+        }
+        cout<<endl;
+    }
+    //dialogue?    
         switch (location)
         {
         case 1:
@@ -278,25 +582,9 @@ int main (){
         }
     } while (continuar == true);
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    return 0;
+return 0;
 }     
-//funcion print mapa
-/*
+/*funcion print mapa
     for (int i = 0; i < mapaAncho; i++){
         for (int j = 0; j < mapaAlto; j++){
             cout<<mapa[i][j];
