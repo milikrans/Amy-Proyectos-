ee9c447a24d1b391c0142c1843bb5b09393937f4
milikrans <milikrans05@gmail.com>
2023-11-27 16:01:39 -0600

    27/11/2023 version lista para entrega, juego en .cpp funcional y mejor manejo de directorios

--- Hello World.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "hello world"<< endl;
-    return 0;
-}
-//este es solo el inicio de del codigo ya que aun no sabemos como tendrá que ser el proyecto
